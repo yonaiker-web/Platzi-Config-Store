6c633361f4b2262ad44d57e2fbbeabed82408dad
yonaiker <yonaiker.ocando@unidigital.global>
2022-09-19 16:31:12 -0400

    componente de payment y success

--- src/containers/Payment.jsx
+++ src/containers/Payment.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import '../styles/components/Payment.css';
 
 const Payment = () => {
-  return <div>Payment</div>;
+  return (
+    <div className="Payment">
+      <div className="Payment-content">
+        <h3>Resumen del pedido:</h3>
+        <div className="Payment-button">Boton de pago con Paypal</div>
+      </div>
+      <div />
+    </div>
+  );
 };
 
 export default Payment;
--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import '../styles/components/Success.css';
 
 const Success = () => {
-  return <div>Success</div>;
+  return (
+    <div className="Success">
+      <div className="Success-content">
+        <h2>Yonaiker, Gracias por tu compra</h2>
+        <span>Tu pedido llegara en 3 dias a tu direccion:</span>
+        <div className="Success-map">Google Maps</div>
+      </div>
+    </div>
+  );
 };
 
 export default Success;
