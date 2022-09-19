69b8bac0647533ea6a46ebd29af551fb76a29875
yonaiker <yonaiker.ocando@unidigital.global>
2022-09-19 15:36:20 -0400

    Componente informacion del usuario antes de pagar

--- src/containers/Information.jsx
+++ src/containers/Information.jsx
@@ -1,7 +1,44 @@
 import React from 'react';
+import '../styles/components/Information.css';
 
 const Information = () => {
-  return <div>Information</div>;
+  return (
+    <div className="Information">
+      <div className="Information-content">
+        <div className="Information-head">
+          <h2>Informacion de contacto: </h2>
+        </div>
+        <div className="Information-form">
+          <form action="">
+            <input type="text" placeholder="Nombre Completo" name="name" />
+            <input type="text" placeholder="Correo Electronico" name="email" />
+            <input type="text" placeholder="Direccion" name="address" />
+            <input type="text" placeholder="Apto" name="apto" />
+            <input type="text" placeholder="Ciudad" name="city" />
+            <input type="text" placeholder="Pais" name="country" />
+            <input type="text" placeholder="Estado" name="state" />
+            <input type="text" placeholder="Codigo Postal" name="cp" />
+            <input type="text" placeholder="Telefono" name="phone" />
+          </form>
+        </div>
+
+        <div className="Information-buttons">
+          <div className="Information-back">Regresar</div>
+          <div className="Information-next">Pagar</div>
+        </div>
+      </div>
+
+      <div className="Information-sidebar">
+        <h3>Pedidos: </h3>
+        <div className="Information-item">
+          <div className="Information-element">
+            <h4>IMTE NAME</h4>
+            <span>$10</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Information;
