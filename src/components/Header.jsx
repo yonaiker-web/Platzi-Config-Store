fd625a78b235a858a96747d71ceb1325321c5212
yonaiker <yonaiker.ocando@unidigital.global>
2022-09-21 16:28:48 -0400

    funcion para agregar al carrito (context)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
 import '../styles/components/Header.css';
 import '../styles/components/Icons.css';
 
+import AppContext from '../context/AppContext';
+
 const Header = () => {
+  const { state } = useContext(AppContext);
+  const { cart } = state;
+
   return (
     <div className="Header">
       <h1 className="Header-title">
@@ -13,8 +18,11 @@ const Header = () => {
       <div className="Header-checkout">
         {/* usamos link para redirigir a otros componenetes  */}
         <Link to="/Checkout">
-          <i class="iconsL fa-solid fa-cart-arrow-down"></i>
+          <i class="iconsM fa-solid fa-cart-arrow-down"></i>
         </Link>
+        {cart.length > 0 && (
+          <div className="Header-alert iconsS">{cart.length}</div>
+        )}
       </div>
     </div>
   );
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Product from './Product';
 import '../styles/components/Products.css';
 
-const Products = ({ products }) => {
+import AppContext from '../context/AppContext';
+
+const Products = () => {
+  //destructuramos los elementos que vamos a usar del contexto
+  const { state, addToCart } = useContext(AppContext);
+
+  //sacmos los productos que vienen en el estado
+  const { products } = state;
+
+  //funcion para agregar al carrtio, proveniente del c
+  const handleAddToCart = (product) => {
+    addToCart(product);
+  };
+
   return (
     <div className="Products">
       <div className="Products-items">
         {products.map((product) => (
-          <Product key={product.id} product={product} />
+          <Product
+            key={product.id}
+            product={product}
+            handleAddToCart={handleAddToCart}
+          />
         ))}
       </div>
     </div>
